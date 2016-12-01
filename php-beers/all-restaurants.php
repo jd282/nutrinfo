1e0887e46986d67dda5db57ef431519702698605
AdemolaOlayinka <aoo12@duke.edu>
2016-12-01 14:56:06 -0500

    Add files via upload
    Designed all-restaurants page

--- php-beers/all-restaurants.php
+++ php-beers/all-restaurants.php
@@ -15,47 +15,71 @@ if (!isset($_POST['student'])) {
 ?>
 
 <html>
-<head><title>All Restaurants on Campus</title></head>
-<body>
-<h1>All Restaurants on Campus</h1>
+<head>
+  <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+  
+  <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1.0'>
+  <title>Nutri-info</title>
+  <title>All Restaurants on Campus</title>
+  <!-- CSS  -->
+  <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+  <link href='./materialize/css/icon' rel='stylesheet'>
+  <link href='./materialize/css/materialize.min.css' type='text/css' rel='stylesheet' media='screen,projection'>
+  <link href='./materialize/css/materialize.css' type='text/css' rel='stylesheet' media='screen,projection'>
+  <style>img.chromoji { width:1em !important; height:1em !important; }.chromoji-font, #chromoji-font { font-size:1em !important; }</style>
 
-<?php
-  try {
-    // Including connection info (including database password) from outside
-    // the public HTML directory means it is not exposed by the web server,
-    // so it is safer than putting it directly in php code:
-    include("/etc/php5/pdo-mine.php");
-    $dbh = dbconnect();
-  } catch (PDOException $e) {
-    print "Error connecting to the database: " . $e->getMessage() . "<br/>";
-    die();
-  }
-  try {
-    $st = $dbh->query('SELECT * FROM Restaurant ');
-    if (($myrow = $st->fetch())) {
-?>
-<form method="post" action="restaurant-info.php">
-Select where you ate:<br/>
-<?php
-      do {
-        // echo produces output HTML:
-        echo "<input type='radio' name='restaurantid' value='" . $myrow[0] . "'required/>";
-        echo $myrow[2] . "<br/>";
-      } while ($myrow = $st->fetch());
-      // Below we will see the use of a "short open tag" that is equivalent
-      // to echoing the enclosed expression.
-?>
-<?= $st->rowCount() ?> restaurants(s) found in the database.<br/>
-<input type="submit" value="NEXT!"/>
-</form>
-<?php
-    } else {
-      echo "There is no restaurants in the database.";
-    }
-  } catch (PDOException $e) {
-    print "Database error: " . $e->getMessage() . "<br/>";
-    die();
-  }
-?>
+</head>
+<body>
+  <nav id='topbar'></nav>
+  <div id='container'>
+    <h1>All Restaurants on Campus</h1>
+
+    <?php
+      try {
+        // Including connection info (including database password) from outside
+        // the public HTML directory means it is not exposed by the web server,
+        // so it is safer than putting it directly in php code:
+        include("/etc/php5/pdo-mine.php");
+        $dbh = dbconnect();
+      } catch (PDOException $e) {
+        print "Error connecting to the database: " . $e->getMessage() . "<br/>";
+        die();
+      }
+      try {
+        $st = $dbh->query('SELECT * FROM Restaurant ');
+        if (($myrow = $st->fetch())) {
+    ?>
+    <form method="post" action="restaurant-info.php">
+    Select where you ate:<br/>
+    <?php
+          do {
+            // echo produces output HTML:
+            echo "<input type='radio' name='restaurantid' value='" . $myrow[0] . "'required/>";
+            echo $myrow[2] . "<br/>";
+          } while ($myrow = $st->fetch());
+          // Below we will see the use of a "short open tag" that is equivalent
+          // to echoing the enclosed expression.
+    ?>
+    <?= $st->rowCount() ?> restaurants(s) found in the database.<br/>
+    <input type="submit" value="NEXT!"/>
+    </form>
+    <?php
+        } else {
+          echo "There is no restaurants in the database.";
+        }
+      } catch (PDOException $e) {
+        print "Database error: " . $e->getMessage() . "<br/>";
+        die();
+      }
+    ?>
+  </div>
+  <nav id='bottombar'></nav>
+  <!--  Scripts-->
+  <script src='loggedInBars.js'></script>
+  <!-- // <script src='./materialize/js/jquery-2.1.1.min.js'></script> -->
+  <script src='https://code.jquery.com/jquery-2.1.1.min.js'></script>
+  <script src='./materialize/js/materialize.js'></script>
+  <script src='./materialize/js/materialize.min.js'></script>
+  <script src='./materialize/js/init.js'></script>
 </body>
 </html>
