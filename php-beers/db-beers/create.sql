47bc103ca4339a574b8d53fffbadaeb556c1729b
BaoDoan96 <baod96@yahoo.com>
2016-12-13 13:43:39 -0500

    Update create.sql
    added comments on Users table

--- php-beers/db-beers/create.sql
+++ php-beers/db-beers/create.sql
@@ -8,8 +8,8 @@ user_lastname VARCHAR(50) NOT NULL,
 user_dob DATE NOT NULL,
 user_registered TIMESTAMP NOT NULL
 user_sex CHAR(1) CHECK (user_sex IS NOT NULL AND user_sex IN ('M', 'F') ),
-user_height INTEGER NOT NULL CHECK(user_height > 0 AND user_height <= 96),
-user_weight INTEGER NOT NULL CHECK(user_weight > 0 AND user_weight <= 1500),
+user_height INTEGER NOT NULL CHECK(user_height > 0 AND user_height <= 96), --measured in inches
+user_weight INTEGER NOT NULL CHECK(user_weight > 0 AND user_weight <= 1500), --measured in pounds
 );
 
 CREATE TABLE Location
